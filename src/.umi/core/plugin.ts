26fe3af34956ecbc93609ee5435d8d33b7f5115c
kagawagao <kingsongao1221@gmail.com>
2020-09-14 15:45:34 +0800

    chore: ignore umi output

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/kagawa/workspace/github/schema-form/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
